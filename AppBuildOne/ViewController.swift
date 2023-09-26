aa1ca62bad9098074d8d7e8a449887104822fbc1
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-09-25 21:17:12 -1000

    Initial Commit

---
+++ AppBuildOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBuildOne
+//
+//  Created by Sean Taylor on 9/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
